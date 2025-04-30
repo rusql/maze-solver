dabb4ea3754b65323086c80d8a066f93e5381d0b
rusql <russell.subscriptions@gmail.com>
2025-04-30 10:02:52 +0200

    Add drawing functionality

--- graphics.py
+++ graphics.py
@@ -1,6 +1,30 @@
 from tkinter import Tk, BOTH, Canvas
 
 
+class Point:
+    def __init__(self, x:float, y:float):
+        self.x: float = x
+        self.y: float = y
+
+
+class Line:
+    def __init__(self, p1: Point, p2: Point, width: int = 2):
+        self.__point1: Point = p1
+        self.__point2: Point = p2
+        self.__width = width
+
+    def draw(self, canvas: Canvas, fill_color: str):
+        #canvas.create_line(1, 1, 2, 2, fill=fill_color, width=2)
+        canvas.create_line(
+            self.__point1.x,
+            self.__point1.y,
+            self.__point2.x,
+            self.__point2.y,
+            fill=fill_color,
+            width=self.__width,
+        )
+
+
 class Window:
 
     def __init__(self, width: int, height: int):
@@ -10,7 +34,7 @@ class Window:
         self.__canvas.pack()
         self.__is_running = False
         self.__root.protocol("WM_DELETE_WINDOW", self.close)
-        
+
     def redraw(self):
         self.__root.update_idletasks()
         self.__root.update()
@@ -23,4 +47,8 @@ class Window:
 
 
     def close(self):
-        self.__is_running = False  
+        self.__is_running = False
+
+
+    def draw_line(self, line: Line, fill_color:str):
+        line.draw(self.__canvas, fill_color)
--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-from graphics import Window
+from graphics import Window, Line, Point
 
 def main():
     win = Window(800,600)
+    win.draw_line(Line(Point(100, 100), Point(200, 200)), "white")
+    win.draw_line(Line(Point(50, 50), Point(50, 500)), "white")
+    #win.draw_line(Line(Point(1, 1), Point(100, 100)), "white")
     win.wait_for_close()
 
 if __name__ == "__main__":
