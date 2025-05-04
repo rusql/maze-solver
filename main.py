e9d5bee0c8ce3c7c1764b3bbd9373c71e09d9858
rusql <russell.subscriptions@gmail.com>
2025-05-04 12:42:28 +0200

    Code refactoring

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
-from cell import Cell
-from graphics import Point, Window
+from graphics import Window
 from maze import Maze
 
 def main():
     margin = 4
-    win = Window(800, 600)
-    Maze(x1=margin, y1=margin, num_rows=5, num_cols=2, cell_size_x=10, cell_size_y=10, win=win)
+    win = Window(800,600)
+    Maze(x1=margin, y1=margin, num_rows=5, num_cols=5, cell_size_x=50, cell_size_y=50, win=win)
     win.wait_for_close()
 
 if __name__ == "__main__":
